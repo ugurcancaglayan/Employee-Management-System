8503fa4e0c8a8168f5841702dd823f3df996f67c
Uğurcan Çağlayan <ugurcancaglayan@outlook.com>
2022-03-05 23:46:59 +0300

    Builder annotation added.

--- src/main/java/com/caglayan/ems/model/Employee.java
+++ src/main/java/com/caglayan/ems/model/Employee.java
@@ -2,6 +2,7 @@ package com.caglayan.ems.model;
 
 import com.caglayan.ems.model.baseModel.BaseEntity;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -9,6 +10,7 @@ import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+@Builder
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -18,7 +20,7 @@ public class Employee extends BaseEntity {
 
     private String name;
     private String mail;
-    private long phoneNumber;
+    private String phoneNumber;
 
     @OneToMany
     @JoinColumn(name = "employee_id")
