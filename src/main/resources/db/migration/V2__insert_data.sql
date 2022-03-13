3a399c43f88bfec7e2b0e1db34c7f934da23fd06
Uğurcan Çağlayan <ugurcancaglayan@outlook.com>
2022-03-13 20:30:01 +0300

    Property changed.

--- src/main/resources/db/migration/V2__insert_data.sql
+++ src/main/resources/db/migration/V2__insert_data.sql
@@ -6,8 +6,8 @@ insert into manager (creation_date, last_modified_date, name)
 insert into department (creation_date, last_modified_date, declaration, name, manager_id)
             values (CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'Developers - South Department', 'IT', 1);
 
-insert into address (creation_date, last_modified_date, location, title, employee_id)
-            values (CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'Istanbul', 'Work', 1);
-
 insert into employee (creation_date, last_modified_date, mail, name, phone_number, department_id)
             values (CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'ugurcancaglayan@outlook.com', 'Uğurcan', '0555 555 5555', 1);
+
+insert into address (creation_date, last_modified_date, location, title, employee_id)
+            values (CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'Istanbul', 'Work', 1);
